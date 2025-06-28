e72ee164ce7b512b6987a0faf011dda5f4d7a324
Prasanjeet Panwar <ppanwar@cholachariots.com>
2025-06-28 14:28:08 +0530

    Resolved merge conflict in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  // const users = await prisma.user.findMany();
+
   return (
-    <div className="font-bold text-rose-500 text-2xl"> 
-    Hello world!
+    <div className="font-bold text-rose-500 text-2xl">
+      Hello world!
+      {/* {JSON.stringify(users)} */}
 
-    <Button variant={"default"} >
-      Click me
-    </Button>
+      <Button variant="default">
+        Click me
+      </Button>
     </div>
   );
 }
